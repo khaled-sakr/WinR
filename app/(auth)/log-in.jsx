35773b9bca6a56d97d41f7c5735e0ee34141718a
Khaled Sakr <136926417+khaled-sakr@users.noreply.github.com>
2024-07-09 15:26:51 +0300

    first version in W_in_R

---
+++ app/(auth)/log-in.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const logIn = () => {
+  return (
+    <View>
+      <Text>logIn</Text>
+    </View>
+  )
+}
+
+export default logIn
+
---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text className='bg-red-600'>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
+ 
